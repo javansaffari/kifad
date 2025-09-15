7617c63d8c8d07c6ea5fb7df01a4bad9a63e5c78
Mohammad <javansaffari@gmail.com>
2025-09-15 17:18:58 +0330

    feat(blade): investments.index

--- resources/views/tenant/investments/index.blade.php
+++ resources/views/tenant/investments/index.blade.php
@@ -3,5 +3,10 @@
 @section('pageTitle', 'سرمایه‌گذاری‌ها')
 
 @section('content')
-    خوش آمدید به بخش سرمایه‌گذاری‌ها!
+    <div class="flex flex-col items-center justify-center h-96 text-center">
+        <h1 class="text-3xl font-bold mb-4">به زودی...</h1>
+        <p class="text-lg text-slate-500">این بخش به زودی فعال می‌شود و شما می‌توانید سرمایه‌گذاری‌های خود را مدیریت کنید.
+        </p>
+
+    </div>
 @endsection
